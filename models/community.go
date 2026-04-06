dcfd1da357ea08606a712cf147a6c71fe8e36316
Zheng Zhang <1296775258@qq.com>
2026-04-06 15:17:18 +0800

    fix: 为核心ID添加json,string避免前端精度丢失

--- models/community.go
+++ models/community.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Community struct {
-	ID   int64  `json:"id" db:"community_id"`
+	ID   int64  `json:"id,string" db:"community_id"`
 	Name string `json:"name" db:"community_name"`
 }
 
 type CommunityDetail struct {
-	ID           int64  `json:"id" db:"community_id"`
+	ID           int64  `json:"id,string" db:"community_id"`
 	Name         string `json:"name" db:"community_name"`
 	Introduction string `json:"introduction,omitempty" db:"introduction"`
 	// 连接mysql时要加上parseTime=true
